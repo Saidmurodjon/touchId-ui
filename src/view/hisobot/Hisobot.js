ab287a534fb86fad8ef18b279b1a3c02c320110c
kali <kali@kali.kali>
2022-06-16 19:29:03 +0500

    update hisobot

--- src/view/hisobot/Hisobot.js
+++ src/view/hisobot/Hisobot.js
@@ -1,19 +1,29 @@
 import React from "react";
 import Dalolatnoma from "../../components/dalolatnoma/Dalolatnoma";
-
+import "./Hisobot.css";
 export default function Hisobot() {
   return (
     <>
       <div className="">
         <div className="row justify-content-center">
-          <div className="col-md-6">
-            <h3>Xisobot</h3>
-          </div>
-          <div className="col-md-6 float-end">
-            <button className="btn btn-light p-2 float-end ">Filter</button>
+          <div className="col-md-12  d-flex justify-content-center align-items-center">
+            <div className="filter">
+              <div className="row">
+                <div className="col-md-6">
+                  <h3>Xisobot</h3>
+                </div>
+                <div className="col-md-6">
+                  <div className="col-md-6 float-end">
+                    <button className="btn btn-light p-2 float-end ">
+                      Filter
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
 
-          <div className="col-md-10 d-flex justify-content-center">
+          <div className="col-md-12 d-flex justify-content-center">
             <Dalolatnoma />
           </div>
         </div>
