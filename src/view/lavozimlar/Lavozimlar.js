793f283e306201193a4d7c030816d339df83ae82
kali <kali@kali.kali>
2022-06-25 21:14:18 +0500

    lavozim

--- src/view/lavozimlar/Lavozimlar.js
+++ src/view/lavozimlar/Lavozimlar.js
@@ -4,9 +4,8 @@ import { useNavigate } from "react-router-dom";
 import config from "../../config.json";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import './Lavozimlar.css'
 import "./Lavozimlar.css";
-import Navbar from "../../components/navbar/Navbar";
+
 const Lavozimlar = () => {
   const TOKEN = {
     headers: {
@@ -17,34 +16,9 @@ const Lavozimlar = () => {
 
   const [post, setPost] = useState(false);
 
-  const [searchPage, setSearchPage] = useState([]);
-
-
-    useEffect(() => {
-        axios
-            .get(`${config.SERVER_URL}lavozim`)
-            .then((res) => {
-                setLavozim(res.data);
-            })
-            .catch((error) => console.log(error));
-    }, [post]);
-
-    const onClick = async (elem) => {
-        const result = await window.confirm("O'chirilsinmi?");
-        if (result) {
-            lavozimDelete(elem);
-            return;
-  const search = JSON.parse(localStorage.getItem("search"));
-
-  useEffect(() => {
-    const newService = lavozim.filter((elem) =>
-      elem.date.toLowerCase().includes(search.toLowerCase())
-    );
-    setSearchPage(newService);
-  }, []);
   useEffect(() => {
     axios
-      .get(`${config.SERVER_URL}lavozim`,TOKEN)
+      .get(`${config.SERVER_URL}lavozim`)
       .then(
         (res) => {
           setLavozim(res.data);
@@ -69,7 +43,7 @@ const Lavozimlar = () => {
 
   async function lavozimDelete(elem) {
     await axios
-      .delete(`${config.SERVER_URL}lavozim/${elem._id}`,TOKEN)
+      .delete(`${config.SERVER_URL}lavozim/${elem._id}`)
       .then(
         (res) => {
           res.data && alert("O'chirildi");
@@ -85,31 +59,6 @@ const Lavozimlar = () => {
   }
 
   const navigate = useNavigate();
-    async function lavozimlarQoshish(params) {
-        navigate("/lavozimqoshish");
-    }
-    async function lavozimlarEdit(elem) {
-        localStorage.setItem("lavozim", JSON.stringify(elem));
-        navigate(`/lavozim/${elem._id}`);
-    }
-    return (
-        <div className="w-100 px-5 py-2 position-relative">
-            <h2 className="title">Лавозимлар рўйхати</h2>
-            <div className="my-3 position-relative d-flex justify-content-end">
-                <Button name={"Лавозим қўшиш"} ButtonStyle="oq-button" ButtonFunction={lavozimlarQoshish} />
-            </div>
-            <div className="w-100 py-3">
-                {
-                    lavozim.map((elem) => (
-                        <div key={elem._id}>
-                            <Bxlqoshish elem={elem} BxlEdit={lavozimlarEdit} BxlDelet={onClick} />
-                        </div>
-                    ))
-                }
-            </div>
-        </div>
-    );
-}
 
   async function lavozimlarQoshish(params) {
     navigate("/lavozimqoshish");
@@ -119,42 +68,28 @@ const Lavozimlar = () => {
     navigate(`/lavozim/${elem._id}`);
   }
   return (
-    <>
-      <Navbar search={true} />
-      <div className="w-100 px-4 py-2 position-relative">
-        <h2 className="title">Лавозимлар рўйхати</h2>
-        <div className="my-3 position-relative d-flex justify-content-end">
-          <Button
-            name={"Лавозим қўшиш"}
-            ButtonStyle="oq-button"
-            ButtonFunction={lavozimlarQoshish}
-          />
-        </div>
-        <div className="w-100 py-3">
-          {searchPage.length > 0
-            ? searchPage.map((elem) => (
-                <div key={elem._id}>
-                  <Bxlqoshish
-                    elem={elem}
-                    BxlEdit={lavozimlarEdit}
-                    BxlDelet={onClick}
-                  />
-                </div>
-              ))
-            : lavozim.map((elem) => (
-                <div key={elem._id}>
-                  <Bxlqoshish
-                    elem={elem}
-                    BxlEdit={lavozimlarEdit}
-                    BxlDelet={onClick}
-                  />
-                </div>
-              ))}
-        </div>
+    <div className="w-100 px-5 py-2 position-relative">
+      <h2 className="title">Лавозимлар рўйхати</h2>
+      <div className="my-3 position-relative d-flex justify-content-end">
+        <Button
+          name={"Лавозим қўшиш"}
+          ButtonStyle="oq-button"
+          ButtonFunction={lavozimlarQoshish}
+        />
       </div>
-    </>
+      <div className="w-100 py-3">
+        {lavozim.map((elem) => (
+          <div key={elem._id}>
+            <Bxlqoshish
+              elem={elem}
+              BxlEdit={lavozimlarEdit}
+              BxlDelet={onClick}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
   );
 };
-
 
 export default Lavozimlar;
