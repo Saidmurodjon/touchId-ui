a5080f89268da177e5a72b0e12c8a6a55178332d
Eldor <Eldor@example.com>
2022-06-28 17:31:28 +0500

    BBBB

--- src/view/buyurtmalar/BuyurtmaQoshish.js
+++ src/view/buyurtmalar/BuyurtmaQoshish.js
@@ -95,85 +95,96 @@ const BuyurtmaQoshish = () => {
         <Navbar search={true} />
       </div>
       <div className="w-100  px-5 py-2 position-relative">
-        <h4 className="title mt-2">
+        <h4 className="title-1 mt-2">
           Буюртмачи қўшиш
         </h4>
         <div className="page-bg mt-3">
           <div className="position-relative w-100 me-0">
             <i className="bi bi-x pointer" onClick={Close}></i>
           </div>
-          <form action="" className="buyurtmachi-form ps-5 pt-4 pe-5 pb-2"
+          <form action="" className="buyurtmachi-form ps-5 pt-4 pe-5 pb-2 w-100"
             onSubmit={Submit}
           >
-            <div className="d-flex align-items-center justify-content-between form-div my-3 ">
-              <h4 className="form-label  mt-1">Ф.И.Ш.</h4>
-              <input className="form-input-buyurtmachi ps-1 form-control" type="text"
-                name="fish"
-                value={buyrtmachi.fish}
-                onChange={changeHandler}
-              />
+            <div className="mb-3 row">
+              <label for="inputPassword" className="col-sm-2 col-form-label">Ф.И.Ш:</label>
+              <div className="col-sm-10">
+                <input className="form-control form-input-bajaruvchi"
+                  type="text"
+                  name="fish"
+                  value={buyrtmachi.fish}
+                  onChange={changeHandler}
+                />
+              </div>
             </div>
-            <div className="d-flex align-items-center justify-content-between form-div my-3">
-              <h4 className="form-label  mt-1">Бўлим</h4>
-              <select
-                className="ps-1 form-select slect-buyurtma"
-                id="bolim"
-                onChange={changeHandler}
-                value={buyrtmachi.bolim}
-                name="bolim"
-              >
-                {bolim.map((item) => (
-                  <option key={item._id} value={item.name}>
-                    {item.name}
-                  </option>
-                ))}
-              </select>
+            <div className="mb-3 row">
+              <label for="inputPassword" className="col-sm-2 col-form-label">Бўлим:</label>
+              <div className="col-sm-10">
+                <select className="form-select  slect-buyurtma"
+                  id="bolim"
+                  onChange={changeHandler}
+                  value={buyrtmachi.bolim}
+                  name="bolim"
+                >
+                  {bolim.map((item) => (
+                    <option key={item._id} value={item.name}>
+                      {item.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="d-flex align-items-center justify-content-between form-div my-3">
-              <h4 className="form-label  mt-1">Хона.</h4>
-              <select
-                className="ps-1 form-select slect-buyurtma"
-                id="kabinet"
-                onChange={changeHandler}
-                value={buyrtmachi.kabinet}
-                name="kabinet"
-              >
-                {xona.map((item) => (
-                  <option key={item._id} value={item.name}>
-                    {item.name}
-                  </option>
-                ))}
-              </select>
+            <div className="mb-3 row">
+              <label for="inputPassword" className="col-sm-2 col-form-label">Хона:</label>
+              <div className="col-sm-10">
+                <select className="form-select  slect-buyurtma"
+                  id="kabinet"
+                  onChange={changeHandler}
+                  value={buyrtmachi.kabinet}
+                  name="kabinet"
+                >
+                  {xona.map((item) => (
+                    <option key={item._id} value={item.name}>
+                      {item.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="d-flex align-items-center justify-content-between form-div my-3">
-              <h4 className="form-label  mt-1">Лавозими</h4>
-              <select
-                className="ps-1 form-select slect-buyurtma"
-                id="lavozim"
-                onChange={changeHandler}
-                value={buyrtmachi.lavozim}
-                name="lavozim"
-              >
-                {lavozim.map((item) => (
-                  <option key={item._id} value={item.name}>
-                    {item.name}
-                  </option>
-                ))}
-              </select>
+
+            <div className="mb-3 row">
+              <label for="inputPassword" className="col-sm-2 col-form-label">Лавозими:</label>
+              <div className="col-sm-10">
+                <select className="form-select  slect-buyurtma"
+                  id="lavozim"
+                  onChange={changeHandler}
+                  value={buyrtmachi.lavozim}
+                  name="lavozim"
+                >
+                  {lavozim.map((item) => (
+                    <option key={item._id} value={item.name}>
+                      {item.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
             </div>
-            <div className="d-flex align-items-center justify-content-between form-div my-3">
-              <h4 className="form-label  mt-1">Телефон</h4>
-              <input className="form-input-buyurtmachi ps-1 form-control"
-                type="text"
-                placeholder="+998"
-                name="tel"
-                value={buyrtmachi.tel}
-                onChange={changeHandler}
-              />
+
+            <div className="mb-3 row">
+              <label for="inputPassword" className="col-sm-2 col-form-label">Телефон:</label>
+              <div className="col-sm-10">
+                <input className="form-control form-input-bajaruvchi"
+                  id="tel"
+                  type="text"
+                  placeholder="+998"
+                  name="tel"
+                  value={buyrtmachi.tel}
+                  onChange={changeHandler}
+                />
+              </div>
             </div>
             <div className="d-flex justify-content-center">
               <Button
-                name="Қўшиш"
+                name="Йангилаш"
                 ButtonFunction={Send}
                 ButtonStyle="oq-button"
               />
